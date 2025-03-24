89004d20440d13a359620d4033892da97d3750fc
oscarjnz <oscarjimenez1410@gmail.com>
2025-03-24 00:32:17 -0400

    cambios segundo sprint

---
+++ capaNegocios/Acciones/AccionesBases.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using capaDatos.Database;
+using System.Data;
+
+namespace capaNegocios
+{
+    public class AccionesBases
+    {
+     public DbLibraryEntityDataContext dbLibraryContext = new DbLibraryEntityDataContext();
+   
+    }
+}
