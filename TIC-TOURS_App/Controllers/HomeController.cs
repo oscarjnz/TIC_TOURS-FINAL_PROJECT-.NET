16e676465e959ac9d9efd02558be91ad084de09b
Samuel Piña <samuelpinapolanco@gmail.com>
2025-03-30 22:36:23 -0400

    Feat: Upload Frontend, Compra, Cotizacion, GestionPoliza, New Index and Reclamacion

--- TIC-TOURS_App/Controllers/HomeController.cs
+++ TIC-TOURS_App/Controllers/HomeController.cs
@@ -1,30 +1,82 @@
-﻿using System;
+﻿using capaModelo;
+using capaNegocios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static capaNegocios.GestorCompra;
 
 namespace TIC_TOURS_App.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+  
+        
+    public ActionResult Index()
         {
-            return View();
+            GestorInicio gestor = new GestorInicio();
+            var modelo = gestor.ObtenerDatosInicio();
+            return View(modelo);
         }
 
-        public ActionResult About()
+        public class CotizacionController : Controller
         {
-            ViewBag.Message = "Your application description page.";
+            [HttpGet]
+            public ActionResult Cotizacion()
+            {
+                return View(new VistaCotizacionModelo());
+            }
 
-            return View();
+            [HttpPost]
+            public ActionResult Calcular(VistaCotizacionModelo modelo)
+            {
+                GestorCotizacion gestor = new GestorCotizacion();
+                var resultado = gestor.ObtenerCotizacion(modelo);
+                return View("Cotizacion", resultado);
+            }
+        }
+        public class CompraController : Controller
+        {
+            [HttpGet]
+            public ActionResult Compra()
+            {
+                return View(new VistaCompraModelo());
+            }
+
+            [HttpPost]
+            public ActionResult Procesar(VistaCompraModelo compra)
+            {
+                GestorCompra gestor = new GestorCompra();
+                var resultado = gestor.ProcesarCompra(compra);
+                return View("Compra", resultado);
+            }
+        }
+        public class PolizaController : Controller
+        {
+            public ActionResult GestionPolizas()
+            {
+                GestorPoliza gestor = new GestorPoliza();.
+                var modelo = gestor.ObtenerPolizasUsuario("usuarioEjemplo");
+                return View(modelo);
+            }
         }
 
-        public ActionResult Contact()
+        public class ReclamacionController : Controller
         {
-            ViewBag.Message = "Your contact page.";
+            [HttpGet]
+            public ActionResult Reclamacion()
+            {
+                return View(new VistaReclamacionModelo());
+            }
 
-            return View();
+            [HttpPost]
+            public ActionResult Enviar(VistaReclamacionModelo reclamacion)
+            {
+                GestorReclamacion gestor = new GestorReclamacion();
+                var resultado = gestor.EnviarReclamacion(reclamacion);
+                return View("Reclamacion", resultado);
+            }
         }
     }
 }
--- capaNegocios/Acciones/AccionesBases.cs
+++ capaNegocios/Acciones/AccionesBases.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using capaDatos;
+using capaDatos.Database;
+using capaModelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using capaDatos.Database;
-using System.Data;
 
 namespace capaNegocios
 {
@@ -13,4 +12,113 @@ namespace capaNegocios
      public DbLibraryEntityDataContext dbLibraryContext = new DbLibraryEntityDataContext();
    
     }
+
+    public class GestorInicio
+    {
+        private RepositorioInicio _repositorio = new RepositorioInicio();
+
+        public VistaInicioModelo ObtenerDatosInicio()
+        {
+            var modelo = new VistaInicioModelo
+            {
+                TituloPagina = "Bienvenido a Turismo Seguro",
+                MensajeBienvenida = "Disfruta de los mejores seguros para tus viajes",
+                PromocionesDestacadas = _repositorio.ObtenerPromocionesDestacadas()
+            };
+            return modelo;
+        }
+    }
+    public class GestorCotizacion
+    {
+        private RepositorioCotizacion _repositorio = new RepositorioCotizacion();
+
+        public VistaCotizacionModelo ObtenerCotizacion(VistaCotizacionModelo modelo)
+        {
+            if (modelo.FechaSalida >= modelo.FechaRegreso)
+            {
+                modelo.MensajeError = "La fecha de salida debe ser anterior a la fecha de regreso.";
+                return modelo;
+            }
+            modelo.PrecioCotizacion = _repositorio.CalcularCotizacion(modelo.Destino, modelo.FechaSalida, modelo.FechaRegreso, modelo.CantidadViajeros);
+            return modelo;
+        }
+    }
+    public class GestorCompra
+    {
+        private RepositorioCompra _repositorio = new RepositorioCompra();
+
+        public VistaCompraModelo ProcesarCompra(VistaCompraModelo compra)
+        {
+            // Validar que el precio sea mayor a 0.
+            if (compra.Precio <= 0)
+            {
+                compra.MensajeError = "El precio no es válido.";
+                return compra;
+            }
+            bool guardado = _repositorio.GuardarCompra(compra.IdSeguro, compra.Precio, compra.IdUsuario);
+            compra.CompraExitosa = guardado;
+            return compra;
+        }
+    }
+
+    public class GestorPoliza
+    {
+        private RepositorioPoliza _repositorio = new RepositorioPoliza();
+
+        public List<VistaPolizaModelo> ObtenerPolizasUsuario(string idUsuario)
+        {
+            int id;
+            if (!int.TryParse(idUsuario, out id))
+                return new List<VistaPolizaModelo>();
+
+            var listaEntidades = _repositorio.ObtenerPolizasPorUsuario(id);
+
+            var listaModelos = listaEntidades.Select(p => new VistaPolizaModelo
+            {
+                NumeroPoliza = p.id_poliza.ToString(),
+                NombreSeguro = p.tm_seguro != null ? p.tm_seguro.nombre : string.Empty,
+                FechaInicio = p.fecha_inicio,
+                FechaFin = p.fecha_fin,
+                Estado = p.estado
+            }).ToList();
+
+            return listaModelos;
+        }
+    }
+
+    public class GestorReclamacion
+    {
+        private RepositorioReclamacion _repositorio = new RepositorioReclamacion();
+
+        public VistaReclamacionModelo EnviarReclamacion(VistaReclamacionModelo reclamacion)
+        {
+
+            bool resultado = _repositorio.RegistrarReclamacion(reclamacion.IdUsuario, reclamacion.IdPoliza, reclamacion.Descripcion);
+            reclamacion.Estado = resultado ? "En Proceso" : "Error";
+            return reclamacion;
+        }
+
+        public List<VistaReclamacionModelo> ObtenerReclamacionesUsuario(string idUsuario)
+        {
+            int id;
+            if (!int.TryParse(idUsuario, out id))
+                return new List<VistaReclamacionModelo>();
+
+            var listaEntidades = _repositorio.ObtenerReclamacionesPorUsuario(id);
+
+            var listaModelos = listaEntidades.Select(r => new VistaReclamacionModelo
+            {
+                IdReclamacion = r.id_reclamo,
+                Descripcion = r.descripcion,
+                Fecha = r.fecha_reclamo.HasValue ? r.fecha_reclamo.Value : DateTime.MinValue,
+                Estado = r.estado,
+
+                IdUsuario = r.id_usuario,
+                IdPoliza = r.id_poliza
+            }).ToList();
+
+            return listaModelos;
+        }
+    }
+
 }
